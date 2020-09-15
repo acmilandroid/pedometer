5e86121ef88e84091c6fd08cff49ae92cbaaaf91
acmilandroid <basilzhlin@gmail.com>
2020-09-15 19:51:31 -0400

    functionality for cutting all gaits of a single sensor

--- cut/one_cut.sh
+++ cut/one_cut.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # usage: ./one_cut.sh [directory] [cutsteps_executable] [CUT] [STRIDE] [gait] [sensor #]
 # [directory] is top level dir containing all subject files
+# if [gait] is not Regular, Irregular, or SemiRegular, it will combine all gaits
 # combines and all csv files of a single sensor type
 # creates sensor0[#]_cut.txt
 
@@ -17,7 +18,13 @@ for d in $1*; do
     if [ -d "$d" ]; then
         echo "$d"
         ((num++))
-        ./$2 $3 $4 $d"/"$5"/Sensor0"$6".csv" $d"/"$5"/steps.txt" >> sensor0$6_cut.txt
+        if [[ "$5" != "Regular" && "$5" != "Irregular" && "$5" != "SemiRegular" ]]; then
+            ./$2 $3 $4 $d"/Regular/Sensor0"$6".csv" $d"/Regular/steps.txt" >> sensor0$6_cut.txt
+            ./$2 $3 $4 $d"/Irregular/Sensor0"$6".csv" $d"/Irregular/steps.txt" >> sensor0$6_cut.txt
+            ./$2 $3 $4 $d"/SemiRegular/Sensor0"$6".csv" $d"/SemiRegular/steps.txt" >> sensor0$6_cut.txt
+        else
+            ./$2 $3 $4 $d"/"$5"/Sensor0"$6".csv" $d"/"$5"/steps.txt" >> sensor0$6_cut.txt
+        fi
     fi
 done
 
