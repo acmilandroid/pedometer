8355721c90dadad1d5cb4083e5eba567afe14861
acmilandroid <basilzhlin@gmail.com>
2020-09-15 14:31:38 -0400

    fixed all_cut bugs, changed data filename

--- cut/all_cut.sh
+++ cut/all_cut.sh
@@ -2,7 +2,7 @@
 #usage: ./all_cut.sh [directory] [cutsteps_executable] [CUT] [STRIDE]
 #run in top level directory containing subject directories
 #combines and cuts all CSV files
-#creates alldata.txt
+#creates everything_cut.txt
 
 echo "Bash version ${BASH_VERSION}"
 
@@ -13,22 +13,22 @@ fi
 
 num=0
 
-echo "removing alldata.txt..."
-rm alldata.txt
+echo "removing everything_cut.txt..."
+rm everything_cut.txt
 
-for d in $1/*; do
+for d in $1*; do
     if [ -d "$d" ]; then
         echo "$d"
         ((num++))
-        ./$2 $3 $4 $d"/Irregular/Sensor01.csv" $d"/Iregular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/Irregular/Sensor02.csv" $d"/Iregular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/Irregular/Sensor03.csv" $d"/Iregular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/Regular/Sensor01.csv" $d"/Regular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/Regular/Sensor02.csv" $d"/Regular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/Regular/Sensor03.csv" $d"/Regular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/SemiRegular/Sensor01.csv" $d"/SemiRegular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/SemiRegular/Sensor02.csv" $d"/SemiRegular/steps.txt" >> alldata.txt
-        ./$2 $3 $4 $d"/SemiRegular/Sensor03.csv" $d"/SemiRegular/steps.txt" >> alldata.txt
+        ./$2 $3 $4 $d"/Irregular/Sensor01.csv" $d"/Irregular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/Irregular/Sensor02.csv" $d"/Irregular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/Irregular/Sensor03.csv" $d"/Irregular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/Regular/Sensor01.csv" $d"/Regular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/Regular/Sensor02.csv" $d"/Regular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/Regular/Sensor03.csv" $d"/Regular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/SemiRegular/Sensor01.csv" $d"/SemiRegular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/SemiRegular/Sensor02.csv" $d"/SemiRegular/steps.txt" >> everything_cut.txt
+        ./$2 $3 $4 $d"/SemiRegular/Sensor03.csv" $d"/SemiRegular/steps.txt" >> everything_cut.txt
     fi
 done
 
