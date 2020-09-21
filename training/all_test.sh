b87652a25b750f5fb1f62fcbb382b6340cc2f58e
acmilandroid <basilzhlin@gmail.com>
2020-09-21 18:14:16 -0400

    automatic csv file generation for testing

--- training/all_test.sh
+++ training/all_test.sh
@@ -14,9 +14,13 @@ fi
 
 num=0
 
+# remove old training data
 echo "Making temp_training_data directory..."
-rm -r temp_training_data
+rm -rf temp_training_data
 mkdir temp_training_data
+
+# write CSV file of data
+echo "Subject,Gait,Sensor,TP,FP,FN,PPV,Sensitivity,RCA,SDA" > results.csv
 
 # loop through all subdirectories
 for d in $1*; do
@@ -44,9 +48,128 @@ for d in $1*; do
             python3 test_model.py $5 $3 "temp_training_data/sensor0$((sensornum))_regular.txt" $d"/Regular/steps.txt" 0 >> results_all.txt
             python3 test_model.py $5 $3 "temp_training_data/sensor0"$((sensornum))"_irregular.txt" $d"/Irregular/steps.txt" 0 >> results_all.txt
             python3 test_model.py $5 $3 "temp_training_data/sensor0"$((sensornum))"_semiregular.txt" $d"/SemiRegular/steps.txt" 0  >> results_all.txt
-            ((num++))
         done
+        ((num++))
     fi
 done
 
-echo "$((num)) directories tested."
+# grab important result data and make temp file
+pcregrep -M "TP:.*\nFP:.*\nFN:.*\nPPV:.*\nSensitivity:.*\nRun count accuracy:.*\nStep detection accuracy F1 Score:.*" results_all.txt | sed 's/^.*: //' > important_results.txt
+
+line=0
+# write data to csv file
+for (( i = 0; i < $num; i++ )) do
+
+    ((print = i + 1 ))
+    echo "Subject $print"
+    
+    for (( j = 0; j < 3; j++ )) do
+
+        # get line index
+        ((line = 63 * $i + 21 * $j))
+
+        # regular sensor data
+        ((print = i + 1 ))
+        echo -n "$print," >> results.csv
+        echo -n "regular," >> results.csv
+        ((print = j + 1 ))
+        echo -n "$print," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+
+        # irregular sensor data
+        ((print = i + 1 ))
+        echo -n "$print," >> results.csv
+        echo -n "irregular," >> results.csv
+        ((print = j + 1 ))
+        echo -n "$print," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+
+        # semiregular sensor data
+        ((print = i + 1 ))
+        echo -n "$print," >> results.csv
+        echo -n "semiregular," >> results.csv
+        ((print = j + 1 ))
+        echo -n "$print," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+        truncate -s -1 results.csv
+        echo -n "," >> results.csv
+        ((line++))
+        sed "${line}q;d" important_results.txt >> results.csv
+
+    done
+done
+
+rm important_results.txt
+
+echo "$((num)) subjects tested."
