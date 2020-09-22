e36c6734a4765de3d6cef52e21c562df8741a18b
acmilandroid <basilzhlin@gmail.com>
2020-09-22 14:19:42 -0400

    histogram

--- cut/balance_data.py
+++ cut/balance_data.py
@@ -15,8 +15,12 @@ if len(sys.argv) != 3:
 import warnings
 import numpy as np
 import random
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 # open file
+print("Loading file...")
 fpt = open(sys.argv[1], 'r')
 data = [[float(x) for x in line.split()] for line in fpt]
 fpt.close()
@@ -29,7 +33,21 @@ values = len(np.unique(data[:,0]))
 print("Samples in original data:", samples)
 print("Number of different steps:", values)
 
+# plot histogram of steps
+print("Plotting histogram...")
+histdata = data[:,0]
+d = np.diff(np.unique(histdata)).min()
+left_of_first_bin = histdata.min() - float(d)/2
+right_of_last_bin = histdata.max() + float(d)/2
+figure = plt.hist(histdata, np.arange(left_of_first_bin, right_of_last_bin + d, d), edgecolor='black', linewidth=1.2)
+plt.xticks(np.arange(min(histdata), max(histdata)+1, 1.0))
+plt.title("Histogram of Steps")
+plt.xlabel("Steps")
+plt.ylabel("Frequency")
+plt.savefig("histogram.png")
+
 # find percentage of labels and minimum
+print("calculating percentages...")
 count = [0] * values
 total = 0
 percent = [0] * values
@@ -51,13 +69,14 @@ print("Maximum samples for each step count:", samples_per_count)
 
 # print percentages of each step
 for i in range(0, values):
-    print("Count Before", i, "is", count[i])
+    print("Count", i, "is", count[i])
     percent[i] = count[i] / total * 100
-    print("Percent Before", i, "is %.2f" %(percent[i]))
+    print("Percent", i, "is %.2f" %(percent[i]))
     if percent[i] < smallest:
         smallest = percent[i]
 
 # randomly choose data to make uniform
+print("Choosing data uniformly...")
 shuffled_data = []
 shuffled_count = [0] * values
 for i in range(0, samples):
@@ -70,11 +89,13 @@ print("Samples in redistributed data:", len(shuffled_data))
 print("Number of different steps:", values)
 
 # write shuffled data to output file
+print("Writing data to file...")
 fpt = open(sys.argv[2], 'w')
 np.savetxt(fpt, shuffled_data, fmt='%.3f', delimiter='\t')
 fpt.close()
 
 # evaluate uniformity of shuffled data
+print("Evaluating distribution...")
 fpt = open(sys.argv[2], 'r')
 data = [[float(x) for x in line.split()] for line in fpt]
 fpt.close()
