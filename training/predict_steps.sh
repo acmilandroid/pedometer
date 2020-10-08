ee923efe0dba99fc4cbb175a9bfdf107c7d39da8
acmilandroid <basilzhlin@gmail.com>
2020-10-08 15:52:10 -0400

    added normalization options for predict steps

--- training/predict_steps.sh
+++ training/predict_steps.sh
@@ -3,16 +3,17 @@
 # step counter project
 # tests all 3 sensors in one gait for RCA and SDA using a trained input model [input_model.h5]
 # used to produce predicted step files for viewing in STEPCOUNTERVIEW
-# Usage: ./predict_steps.sh [directory] [window_size] [window_stride] [input_model.h5]
+# Usage: ./predict_steps.sh [directory] [window_size] [window_stride] [input_model.h5] [normalization_type]
 # [directory] is gait dir containing CSV files
+# [normalization_type] 0 for per sensor per axis, 1 for -1.5 to 1.5 gravities
 # cutsteps executable must be compiled in ../cut/cutsteps
 # creates predicted_steps_sensor01.txt predicted_steps_sensor02.txt predicted_steps_sensor03.txt
 
 echo "Bash version ${BASH_VERSION}"
 
 # usage warning
-if [ "$#" -ne 4 ]; then
-    echo "Usage: ./predict_steps.sh [directory] [window_size] [window_stride] [input_model.h5]"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: ./predict_steps.sh [directory] [window_size] [window_stride] [input_model.h5] [normalization_type]"
     exit 1
 fi
 
@@ -36,13 +37,29 @@ do
     ./../cut/cutsteps $2 $3 $1"/Sensor0$((sensornum)).csv" $1"/steps.txt" >> "temp_training_data/sensor0$((sensornum)).txt"
 done
 
-# normalize each sensor
-for sensornum in 1 2 3
-do
-    echo "Normalizing Sensor0$((sensornum))"
-    python3 ../cut/normalize.py "temp_training_data/sensor0$((sensornum)).txt" 0 $((sensornum)) > /dev/null
-    mv data_normalized_sensor0$((sensornum)).txt temp_training_data/sensor0$((sensornum))_normalized.txt
-done
+# normalize per axis per sensor
+if (($5 == 0)); then
+    echo "Normalizing per axis per sensor"
+    # normalize each sensor
+    for sensornum in 1 2 3
+    do
+        echo "Normalizing Sensor0$((sensornum))"
+        python3 ../cut/normalize.py "temp_training_data/sensor0$((sensornum)).txt" 0 $((sensornum)) > /dev/null
+        mv data_normalized_sensor0$((sensornum)).txt temp_training_data/sensor0$((sensornum))_normalized.txt
+    done
+fi
+
+# normalize from -1.5 to 1.5 gravities
+if (($5 == 1)); then
+    echo "Normalizing from -1.5 to 1.5 gravities"
+    # normalize each sensor
+    for sensornum in 1 2 3
+    do
+        echo "Normalizing Sensor0$((sensornum))"
+        python3 ../cut/normalize.py "temp_training_data/sensor0$((sensornum))_regular.txt" 1 > /dev/null
+        mv data_normalized_constant.txt temp_training_data/sensor0$((sensornum))_normalized.txt
+    done
+fi
 
 # test each sensor and produce predicted steps
 for sensornum in 1 2 3
