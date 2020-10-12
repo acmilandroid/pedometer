252fa116a10bc2d9a5e22af7a9ee59fc0f3ba79c
acmilandroid <basilzhlin@gmail.com>
2020-10-12 13:43:07 -0400

    sensorXcut improvements

--- cut/sensorXcut.sh
+++ cut/sensorXcut.sh
@@ -15,6 +15,14 @@ if [ "$#" -ne 5 ]; then
     exit 1
 fi
 
+rm sensor0* &> /dev/null
+
+if [[ "$4" != "Regular" && "$4" != "Irregular" && "$4" != "SemiRegular" ]]; then
+    echo "No gait, cutting sensor $5 only"
+else
+    echo "cutting gait $4, sensor $5"
+fi
+
 num=0
 
 for d in $1*; do
@@ -26,7 +34,7 @@ for d in $1*; do
             ./cutsteps $2 $3 $d"/Irregular/Sensor0"$5".csv" $d"/Irregular/steps.txt" >> sensor0$5_cut.txt
             ./cutsteps $2 $3 $d"/SemiRegular/Sensor0"$5".csv" $d"/SemiRegular/steps.txt" >> sensor0$5_cut.txt
         else
-            ./cutsteps $2 $3 $d"/"$4"/Sensor0"$5".csv" $d"/"$4"/steps.txt" >> sensor0$5_cut.txt
+            ./cutsteps $2 $3 $d"/"$4"/Sensor0"$5".csv" $d"/"$4"/steps.txt" >> sensor0$5_$4_cut.txt
         fi
     fi
 done
