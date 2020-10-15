4ac14a1bbcbf0bb2abdc7f1731b82cf3568abcb0
acmilandroid <basilzhlin@gmail.com>
2020-10-15 18:55:46 -0400

    don't need to loop through directory

--- cut/per_gait_per_sensor_cutbalnorm.sh
+++ cut/per_gait_per_sensor_cutbalnorm.sh
@@ -14,47 +14,41 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-for d in $1*; do
-    if [ -d "$d" ]; then
-        echo "$d"
+# cut data
+echo "cutting data..."
+./sensorXcut.sh $d $2 $3 Regular 1 &> /dev/null
+./sensorXcut.sh $d $2 $3 Regular 2 &> /dev/null
+./sensorXcut.sh $d $2 $3 Regular 3 &> /dev/null
+./sensorXcut.sh $d $2 $3 SemiRegular 1 &> /dev/null
+./sensorXcut.sh $d $2 $3 SemiRegular 2 &> /dev/null
+./sensorXcut.sh $d $2 $3 SemiRegular 3 &> /dev/null
+./sensorXcut.sh $d $2 $3 Irregular 1 &> /dev/null
+./sensorXcut.sh $d $2 $3 Irregular 2 &> /dev/null
+./sensorXcut.sh $d $2 $3 Irregular 3 &> /dev/null
 
-        # cut data
-        echo "cutting data in $d..."
-        ./sensorXcut.sh $d $2 $3 Regular 1 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 Regular 2 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 Regular 3 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 SemiRegular 1 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 SemiRegular 2 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 SemiRegular 3 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 Irregular 1 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 Irregular 2 &> /dev/null
-        ./sensorXcut.sh $d $2 $3 Irregular 3 &> /dev/null
+# balance data
+echo "balancing data..."
+python3 balance_data.py ALL_Regular_1_cut.txt ALL_Regular_1_bal.txt &> /dev/null
+python3 balance_data.py ALL_Regular_2_cut.txt ALL_Regular_2_bal.txt &> /dev/null
+python3 balance_data.py ALL_Regular_3_cut.txt ALL_Regular_3_bal.txt &> /dev/null
+python3 balance_data.py ALL_SemiRegular_1_cut.txt ALL_SemiRegular_1_bal.txt &> /dev/null
+python3 balance_data.py ALL_SemiRegular_2_cut.txt ALL_SemiRegular_2_bal.txt &> /dev/null
+python3 balance_data.py ALL_SemiRegular_3_cut.txt ALL_SemiRegular_3_bal.txt &> /dev/null
+python3 balance_data.py ALL_Irregular_1_cut.txt ALL_Irregular_1_bal.txt &> /dev/null
+python3 balance_data.py ALL_Irregular_2_cut.txt ALL_Irregular_2_bal.txt &> /dev/null
+python3 balance_data.py ALL_Irregular_3_cut.txt ALL_Irregular_3_bal.tx &> /dev/null
 
-        # balance data
-        echo "balancing data in $d..."
-        python3 balance_data.py ALL_Regular_1_cut.txt ALL_Regular_1_bal.txt &> /dev/null
-        python3 balance_data.py ALL_Regular_2_cut.txt ALL_Regular_2_bal.txt &> /dev/null
-        python3 balance_data.py ALL_Regular_3_cut.txt ALL_Regular_3_bal.txt &> /dev/null
-        python3 balance_data.py ALL_SemiRegular_1_cut.txt ALL_SemiRegular_1_bal.txt &> /dev/null
-        python3 balance_data.py ALL_SemiRegular_2_cut.txt ALL_SemiRegular_2_bal.txt &> /dev/null
-        python3 balance_data.py ALL_SemiRegular_3_cut.txt ALL_SemiRegular_3_bal.txt &> /dev/null
-        python3 balance_data.py ALL_Irregular_1_cut.txt ALL_Irregular_1_bal.txt &> /dev/null
-        python3 balance_data.py ALL_Irregular_2_cut.txt ALL_Irregular_2_bal.txt &> /dev/null
-        python3 balance_data.py ALL_Irregular_3_cut.txt ALL_Irregular_3_bal.tx &> /dev/null
+# normalize data
+echo "normalizing data..."
+python3 normalize.py ALL_Regular_1_bal.txt ALL_Regular_1_norm.txt 0 1 &> /dev/null
+python3 normalize.py ALL_Regular_2_bal.txt ALL_Regular_2_norm.txt 0 2 &> /dev/null
+python3 normalize.py ALL_Regular_3_bal.txt ALL_Regular_3_norm.txt 0 3 &> /dev/null
+python3 normalize.py ALL_SemiRegular_1_bal.txt ALL_SemiRegular_1_norm.txt 0 1 &> /dev/null
+python3 normalize.py ALL_SemiRegular_2_bal.txt ALL_SemiRegular_2_norm.txt 0 2 &> /dev/null
+python3 normalize.py ALL_SemiRegular_3_bal.txt ALL_SemiRegular_3_norm.txt 0 3 &> /dev/null
+python3 normalize.py ALL_Irregular_1_bal.txt ALL_Irregular_1_norm.txt 0 1 &> /dev/null
+python3 normalize.py ALL_Irregular_2_bal.txt ALL_Irregular_2_norm.txt 0 2 &> /dev/null
+python3 normalize.py ALL_Irregular_3_bal.txt ALL_Irregular_3_norm.txt 0 3 &> /dev/null
 
-        # normalize data
-        echo "normalizing data in $d..."
-        python3 normalize.py ALL_Regular_1_bal.txt ALL_Regular_1_norm.txt 0 1 &> /dev/null
-        python3 normalize.py ALL_Regular_2_bal.txt ALL_Regular_2_norm.txt 0 2 &> /dev/null
-        python3 normalize.py ALL_Regular_3_bal.txt ALL_Regular_3_norm.txt 0 3 &> /dev/null
-        python3 normalize.py ALL_SemiRegular_1_bal.txt ALL_SemiRegular_1_norm.txt 0 1 &> /dev/null
-        python3 normalize.py ALL_SemiRegular_2_bal.txt ALL_SemiRegular_2_norm.txt 0 2 &> /dev/null
-        python3 normalize.py ALL_SemiRegular_3_bal.txt ALL_SemiRegular_3_norm.txt 0 3 &> /dev/null
-        python3 normalize.py ALL_Irregular_1_bal.txt ALL_Irregular_1_norm.txt 0 1 &> /dev/null
-        python3 normalize.py ALL_Irregular_2_bal.txt ALL_Irregular_2_norm.txt 0 2 &> /dev/null
-        python3 normalize.py ALL_Irregular_3_bal.txt ALL_Irregular_3_norm.txt 0 3 &> /dev/null
-
-    fi
-done
 
 echo "Done cutting, balancing, and normalizing data. Use ALL_[gait]_[sensor#]_norm.txt for testing."
