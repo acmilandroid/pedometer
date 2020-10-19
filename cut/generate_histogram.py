bbe04609ee4579e9b29bf9f55bae2d6edb14b19d
acmilandroid <basilzhlin@gmail.com>
2020-10-19 15:59:39 -0400

    better histogram generation

--- cut/generate_histogram.py
+++ cut/generate_histogram.py
@@ -17,21 +17,18 @@ import random
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import csv
 
 # open file
 print("Loading file...")
-fpt = open(sys.argv[1], 'r')
-data = [[float(x) for x in line.split()] for line in fpt]
-fpt.close()
+with open(sys.argv[1], newline='\n') as fpt:
+    reader = csv.reader(fpt, delimiter=',')
+    data = list(reader)
 
-# calculate some values
-data = np.array(data)
-samples = len(data)
-diff_steps = len(np.unique(data[:,0]))
-histdata = data[:,0]
-
-print("Samples in original data:", samples)
-print("Number of different steps:", diff_steps)
+histdata = list(line[4] for line in data)           # get column 5 (predicted steps in window)
+histdata.pop(0)                                     # remove string header
+histdata = np.array([float(x) for x in histdata])   # convert to float
+histdata = np.array(histdata)                       # convert to array
 
 # plot histogram of steps
 print("Plotting histogram...")
@@ -39,8 +36,8 @@ d = np.diff(np.unique(histdata)).min()
 left_of_first_bin = histdata.min() - float(d)/2
 right_of_last_bin = histdata.max() + float(d)/2
 plt.figure(1)
-figure = plt.hist(histdata, np.arange(left_of_first_bin, right_of_last_bin + d, d), edgecolor='black', linewidth=1.2)
-plt.xticks(np.arange(min(histdata), max(histdata)+1, 1.0))
+figure = plt.hist(histdata, bins=range(0, 19, 1), edgecolor='black', linewidth=1.2, histtype='stepfilled')
+plt.xticks(np.arange(0, 19, step=1))
 plt.title("Step Histogram Original Data")
 plt.xlabel("Steps in a Window")
 plt.ylabel("Frequency")
