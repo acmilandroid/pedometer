1515ebe860335f4208cdf3a0e72fb17c61f8df0b
acmilandroid <basilzhlin@gmail.com>
2020-10-24 11:43:44 -0400

    change kernel size of model to 15

--- training/train_model.py
+++ training/train_model.py
@@ -71,7 +71,7 @@ if (WEIGHTED == True):
 
 # set up classifier
 model = keras.Sequential([
-    keras.layers.Conv1D(input_shape=(window_size, TOTAL_FEATURES), filters=10, kernel_size=30, strides=5, activation='relu'),
+    keras.layers.Conv1D(input_shape=(window_size, TOTAL_FEATURES), filters=10, kernel_size=15, strides=5, activation='relu'),
     keras.layers.Conv1D(filters=10, kernel_size=5, activation='relu'),
     keras.layers.Flatten(),  # must flatten to feed dense layer
     keras.layers.Dense(1)
