6254ace960acaa3f032bbebb3af1e5cea8b7dad1
acmilandroid <basilzhlin@gmail.com>
2020-10-24 16:35:15 -0400

    uncommented remove temp data

--- window_test/window_test.sh
+++ window_test/window_test.sh
@@ -62,6 +62,6 @@ for ((windowsize=$2; windowsize<=$3; windowsize+=$INCREMENT)); do
 done
 
 # cleanup
-# echo "cleaning up temp files..."
-# cd ../window_test/
-# rm -r temp_training_data &> /dev/null
+echo "cleaning up temp files..."
+cd ../window_test/
+rm -r temp_training_data &> /dev/null
