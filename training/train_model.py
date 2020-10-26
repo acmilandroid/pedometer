2bc7e49e9fbdbee06b45b053a647d494305a61d3
acmilandroid <basilzhlin@gmail.com>
2020-10-26 14:39:28 -0400

    change back to 200 epochs, got rid of old temp files

--- training/train_model.py
+++ training/train_model.py
@@ -91,7 +91,7 @@ print("Training...")
 if (WEIGHTED == True):
     metrics = model.fit(features_input, labels, epochs=200, verbose=2, callbacks=[es], class_weight=class_weight)
 else:
-    metrics = model.fit(features_input, labels, epochs=1, verbose=2, callbacks=[es])
+    metrics = model.fit(features_input, labels, epochs=200, verbose=2, callbacks=[es])
 
 # print("Testing")
 # loss, accuracy = model.evaluate(features_input, labels)
