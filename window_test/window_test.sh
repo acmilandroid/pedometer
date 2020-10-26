9a6fba437720cff36f60c32e84d25d6a81f0ae0f
acmilandroid <basilzhlin@gmail.com>
2020-10-26 13:44:19 -0400

    window increment command line argument

--- window_test/window_test.sh
+++ window_test/window_test.sh
@@ -2,28 +2,28 @@
 # Basil Lin
 # Step counter project
 # Script to test window sizes for best RCA/SDA
-# Usage: ./window_test.sh [directory] [window_size_start] [window_size_end] [window_stride]
+# Usage: ./window_test.sh [directory] [window_size_start] [window_size_end] [window_increment] [window_stride]
 # [directory] is top level dir containing all subject files
 # [window_size_start] [window_size_end] are in datum, not seconds!
 # creates ALL_[gait]_[sensor#]_cut.txt ALL_[gait]_[sensor#]_cut.txt ALL_[gait]_[sensor#]_cutnorm.txt
 
-# increment window size by 1 second during test
-INCREMENT=15
-
 echo "Bash version ${BASH_VERSION}"
 
 if [ "$#" -ne 4 ]; then
-    echo "Usage: ./window_test.sh [directory] [window_size_start] [window_size_end] [window_stride]"
+    echo "Usage: ./window_test.sh [directory] [window_size_start] [window_size_end] [window_increment] [window_stride]"
     exit 1
 fi
 
 # remove old data
 echo "removing old data..."
 rm -r temp_training_data &> /dev/null
-rm ALL* &> /dev/null
+rm -r models &> /dev/null
+rm -r results &> /dev/null
 
 # create directory for data
 mkdir temp_training_data
+mkdir models
+mkdir results
 
 # compile cutsteps.c
 cd ../cut/
@@ -31,17 +31,16 @@ make clean &> /dev/null
 make &> /dev/null
 
 # loop through iterations of windows
-for ((windowsize=$2; windowsize<=$3; windowsize+=$INCREMENT)); do
+for ((windowsize=$2; windowsize<=$3; windowsize+=$4)); do
 
     echo ".........................TESTING WINDOW SIZE OF $windowsize........................."
     cd ../window_test/
     rm -r temp_training_data/* &> /dev/null
-    mkdir temp_training_data/models
 
     # create data
     echo "cutting and normalizing data..."
     cd ../cut/
-    ./9_cutnorm.sh $1 $windowsize $4 &> /dev/null
+    ./9_cutnorm.sh $1 $windowsize $5 &> /dev/null
     mv *_cut.txt ../window_test/temp_training_data/
     mv *_cutnorm.txt ../window_test/temp_training_data/
 
@@ -50,14 +49,10 @@ for ((windowsize=$2; windowsize<=$3; windowsize+=$INCREMENT)); do
     cd ../training/
     for ((sensor=1; sensor<=3; sensor++)); do
         echo "training Sensor0$sensor..."
-        python3 train_model.py ../window_test/temp_training_data/ALL_Regular_"$sensor"_cutnorm.txt $windowsize $4 ../window_test/temp_training_data/models/ALL_Regular_"$sensor"_model.h5
-        python3 train_model.py ../window_test/temp_training_data/ALL_SemiRegular_"$sensor"_cutnorm.txt $windowsize $4 ../window_test/temp_training_data/models/ALL_SemiRegular_"$sensor"_model.h5
-        python3 train_model.py ../window_test/temp_training_data/ALL_Irregular_"$sensor"_cutnorm.txt $windowsize $4 ../window_test/temp_training_data/models/ALL_Irregular_"$sensor"_model.h5
+        python3 train_model.py ../window_test/temp_training_data/ALL_Regular_"$sensor"_cutnorm.txt $windowsize $5 ../window_test/models/ALL_Regular_"$sensor"_"$windowsize"_model.h5 >> 
+        python3 train_model.py ../window_test/temp_training_data/ALL_SemiRegular_"$sensor"_cutnorm.txt $windowsize $5 ../window_test/models/ALL_SemiRegular_"$sensor"_"$windowsize"_model.h5
+        python3 train_model.py ../window_test/temp_training_data/ALL_Irregular_"$sensor"_cutnorm.txt $windowsize $5 ../window_test/models/ALL_Irregular_"$sensor"_"$windowsize"_model.h5
     done
-
-    # test all data
-    echo "testing all models..."
-    ./9_test.sh $1 $windowsize $4 ../window_test/temp_training_data/models/ 0 ../window_test/ALL_ALL_ALL_9GaitSensor_results_windowsize"$windowsize".csv
 
 done
 
