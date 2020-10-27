c6b4cf9bb97221882db7a8c5d97fa9532343e066
acmilandroid <basilzhlin@gmail.com>
2020-10-27 16:19:46 -0400

    cutnorm adjusted to use dir based on window size

--- cut/9_cutnorm.sh
+++ cut/9_cutnorm.sh
@@ -5,7 +5,7 @@
 # Usage: 9_cutnorm.sh [directory] [window_size] [window_stride]
 # [directory] is top level dir containing all subject files
 # requires cutsteps.c to be compiled as cutsteps in same directory
-# creates ALL_[gait]_[sensor#]_cut.txt ALL_[gait]_[sensor#]_cut.txt ALL_[gait]_[sensor#]_cutnorm.txt
+# creates dir cutnorm{window_size} containing ALL_[gait]_[sensor#]_cut.txt ALL_[gait]_[sensor#]_cut.txt ALL_[gait]_[sensor#]_cutnorm.txt
 
 echo "Bash version ${BASH_VERSION}"
 
@@ -16,8 +16,8 @@ fi
 
 # clean old data
 echo "removing old data..."
-rm *_cut.txt &> /dev/null
-rm *_cutnorm.txt &> /dev/null
+rm -r cutnorm_"$2" &> /dev/null
+mkdir cutnorm_"$2" &> /dev/null
 
 # cut data
 echo "cutting data..."
@@ -27,23 +27,23 @@ for d in $1*; do
         echo "$d"
         ((num++))
         for ((sensor=1; sensor<=3; sensor++)); do
-            ./cutsteps $2 $3 $d"/Regular/Sensor0"$sensor".csv" $d"/Regular/steps.txt" >> "ALL_Regular_"$sensor"_cut.txt"
-            ./cutsteps $2 $3 $d"/SemiRegular/Sensor0"$sensor".csv" $d"/SemiRegular/steps.txt" >> "ALL_SemiRegular_"$sensor"_cut.txt"
-            ./cutsteps $2 $3 $d"/Irregular/Sensor0"$sensor".csv" $d"/Irregular/steps.txt" >> "ALL_Irregular_"$sensor"_cut.txt"
+            ./cutsteps $2 $3 $d"/Regular/Sensor0"$sensor".csv" $d"/Regular/steps.txt" >> "cutnorm_"$2"/ALL_Regular_"$sensor"_cut.txt"
+            ./cutsteps $2 $3 $d"/SemiRegular/Sensor0"$sensor".csv" $d"/SemiRegular/steps.txt" >> "cutnorm_"$2"/ALL_SemiRegular_"$sensor"_cut.txt"
+            ./cutsteps $2 $3 $d"/Irregular/Sensor0"$sensor".csv" $d"/Irregular/steps.txt" >> "cutnorm_"$2"/ALL_Irregular_"$sensor"_cut.txt"
         done
     fi
 done
 
 # normalize data
 echo "normalizing data..."
-python3 normalize.py ALL_Regular_1_cut.txt ALL_Regular_1_cutnorm.txt 0 1 &> /dev/null
-python3 normalize.py ALL_Regular_2_cut.txt ALL_Regular_2_cutnorm.txt 0 2 &> /dev/null
-python3 normalize.py ALL_Regular_3_cut.txt ALL_Regular_3_cutnorm.txt 0 3 &> /dev/null
-python3 normalize.py ALL_SemiRegular_1_cut.txt ALL_SemiRegular_1_cutnorm.txt 0 1 &> /dev/null
-python3 normalize.py ALL_SemiRegular_2_cut.txt ALL_SemiRegular_2_cutnorm.txt 0 2 &> /dev/null
-python3 normalize.py ALL_SemiRegular_3_cut.txt ALL_SemiRegular_3_cutnorm.txt 0 3 &> /dev/null
-python3 normalize.py ALL_Irregular_1_cut.txt ALL_Irregular_1_cutnorm.txt 0 1 &> /dev/null
-python3 normalize.py ALL_Irregular_2_cut.txt ALL_Irregular_2_cutnorm.txt 0 2 &> /dev/null
-python3 normalize.py ALL_Irregular_3_cut.txt ALL_Irregular_3_cutnorm.txt 0 3 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_Regular_1_cut.txt cutnorm_"$2"/ALL_Regular_1_cutnorm.txt 0 1 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_Regular_2_cut.txt cutnorm_"$2"/ALL_Regular_2_cutnorm.txt 0 2 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_Regular_3_cut.txt cutnorm_"$2"/ALL_Regular_3_cutnorm.txt 0 3 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_SemiRegular_1_cut.txt cutnorm_"$2"/ALL_SemiRegular_1_cutnorm.txt 0 1 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_SemiRegular_2_cut.txt cutnorm_"$2"/ALL_SemiRegular_2_cutnorm.txt 0 2 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_SemiRegular_3_cut.txt cutnorm_"$2"/ALL_SemiRegular_3_cutnorm.txt 0 3 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_Irregular_1_cut.txt cutnorm_"$2"/ALL_Irregular_1_cutnorm.txt 0 1 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_Irregular_2_cut.txt cutnorm_"$2"/ALL_Irregular_2_cutnorm.txt 0 2 &> /dev/null
+python3 normalize.py cutnorm_"$2"/ALL_Irregular_3_cut.txt cutnorm_"$2"/ALL_Irregular_3_cutnorm.txt 0 3 &> /dev/null
 
 echo "Done cutting and normalizing data. Use ALL_[gait]_[sensor#]_cutnorm.txt for testing."
