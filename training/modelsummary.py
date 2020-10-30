eabbf761bb252adc72e5b696af806219222e186a
acmilandroid <basilzhlin@gmail.com>
2020-10-30 16:02:22 -0400

    removed old files, updated modelsummary

--- training/modelsummary.py
+++ training/modelsummary.py
@@ -1,27 +1,21 @@
 # Basil Lin
 # step counter project
-# program to test model with window_size input file
-# Usage: python3 test_model.py [model_name.h5] [window_size] [input_file.txt] [steps.txt] [print 0|1] [debug.csv]
-# print input allows you to print predicted_step_indices for STEPCOUNTERVIEW
-# input file must be first cut and normalized
-# if [debug.csv] is populated, a debug file showing predictions for each window will be created as [debug.csv]
-# if [debug.csv] exists, it will be appended. If not, it will be created
+# program to print model summary
+# Usage: python3 test_model.py [model_name.h5]
 
 # import system for command line arguments
 import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python3 test_model.py [model_name.h5]")
 
 # import other stuff so I don't slow down the Usage warning
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning) #suppresses future warnings
 import logging
 logging.getLogger('tensorflow').disabled = True
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
-import time
-import csv
 
 print("Tensorflow version:", tf.__version__)
 print("Python version:", sys.version)
