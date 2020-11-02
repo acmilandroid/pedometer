5a19736284d79af2266652d408c9caa4db676008
acmilandroid <basilzhlin@gmail.com>
2020-11-02 16:09:54 -0500

    relative reference to train and test python scripts

--- window_test/9_test.sh
+++ window_test/9_test.sh
@@ -39,11 +39,11 @@ for (( window_size=$WINDOW_START; window_size<=$WINDOW_END; window_size+=$WINDOW
 			# test models (25-30 will be withheld test group results)
 			for (( sensor=1; sensor<=3; sensor++ )); do
 				echo "Testing testing data {Regular, Sensor0$sensor}"
-				python3 test_model.py $2/ALL_Regular_"$sensor"_"$window_size"_model.h5 $window_size $1/cutnorm_"$window_size"/"$num"_Regular_"$sensor"_cutnorm.txt $d/Regular/steps.txt 0 >> temp_data/test_results_$window_size.txt
+				python3 ../training/test_model.py $2/ALL_Regular_"$sensor"_"$window_size"_model.h5 $window_size $1/cutnorm_"$window_size"/"$num"_Regular_"$sensor"_cutnorm.txt $d/Regular/steps.txt 0 >> temp_data/test_results_$window_size.txt
 				echo "Testing testing data {SemiRegular, Sensor0$sensor}"
-				python3 test_model.py $2/ALL_SemiRegular_"$sensor"_"$window_size"_model.h5 $window_size $1/cutnorm_"$window_size"/"$num"_SemiRegular_"$sensor"_cutnorm.txt $d/SemiRegular/steps.txt 0 >> temp_data/test_results_$window_size.txt
+				python3 ../training/test_model.py $2/ALL_SemiRegular_"$sensor"_"$window_size"_model.h5 $window_size $1/cutnorm_"$window_size"/"$num"_SemiRegular_"$sensor"_cutnorm.txt $d/SemiRegular/steps.txt 0 >> temp_data/test_results_$window_size.txt
 				echo "Testing testing data {Irregular, Sensor0$sensor}"
-				python3 test_model.py $2/ALL_Irregular_"$sensor"_"$window_size"_model.h5 $window_size $1/cutnorm_"$window_size"/"$num"_Irregular_"$sensor"_cutnorm.txt $d/Irregular/steps.txt 0 >> temp_data/test_results_$window_size.txt
+				python3 ../training/test_model.py $2/ALL_Irregular_"$sensor"_"$window_size"_model.h5 $window_size $1/cutnorm_"$window_size"/"$num"_Irregular_"$sensor"_cutnorm.txt $d/Irregular/steps.txt 0 >> temp_data/test_results_$window_size.txt
 			done
 		fi
 	done
--- window_test/9_train.sh
+++ window_test/9_train.sh
@@ -21,18 +21,17 @@ echo ".........................TESTING WINDOW SIZE OF $2........................
 
 # train models and get result
 echo "training models..."
-cd ../training/
 
 # train models
 for ((sensor=1; sensor<=3; sensor++)); do
 
 	echo "training {Regular, Sensor0$sensor}..."
-	python3 train_model.py $1/cutnorm_$2/training_Regular_"$sensor"_cutnorm.txt $2 $3 ../window_test/models/training_Regular_"$sensor"_"$2"_model.h5
+	python3 ../training/train_model.py $1/cutnorm_$2/training_Regular_"$sensor"_cutnorm.txt $2 $3 ../window_test/models/training_Regular_"$sensor"_"$2"_model.h5
 
 	echo "training {SemiRegular, Sensor0$sensor}..."
-	python3 train_model.py $1/cutnorm_$2/training_SemiRegular_"$sensor"_cutnorm.txt $2 $3 ../window_test/models/training_SemiRegular_"$sensor"_"$2"_model.h5
+	python3 ../training/train_model.py $1/cutnorm_$2/training_SemiRegular_"$sensor"_cutnorm.txt $2 $3 ../window_test/models/training_SemiRegular_"$sensor"_"$2"_model.h5
 
 	echo "training {Irregular, Sensor0$sensor}..."
-	python3 train_model.py $1/cutnorm_$2/training_Irregular_"$sensor"_cutnorm.txt $2 $3 ../window_test/models/training_Irregular_"$sensor"_"$2"_model.h5
+	python3 ../training/train_model.py $1/cutnorm_$2/training_Irregular_"$sensor"_cutnorm.txt $2 $3 ../window_test/models/training_Irregular_"$sensor"_"$2"_model.h5
 
 done
