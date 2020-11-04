49877b67eed272dd304e38a8ba1ab5c387017f0f
acmilandroid <basilzhlin@gmail.com>
2020-11-04 13:22:04 -0500

    change naming convention of fold data

--- cut/create_trainandtest.sh
+++ cut/create_trainandtest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Basil Lin
-# Uses cutnorm data and create testing and training files
+# Uses cutnorm data and create testingfold$3 and training files
 # Usage: ./create_trainandtest.sh [directory] [num_folds] [fold_num]
-# [directory] is top level dir containing all cut and normalized subject data files for testing
+# [directory] is top level dir containing all cut and normalized subject data files for testingfold$3
 # [num_folds] is the total number of folds for the test
-# [fold_num] is the fold number to cut for testing. Smaller folds are earlier data
+# [fold_num] is the fold number to cut for testingfold$3. Smaller folds are earlier data
 
 echo "Bash version ${BASH_VERSION}"
 
@@ -16,12 +16,12 @@ fi
 # remove old files
 cd $1
 for ((sensor=1; sensor<=3; sensor++)); do
-	rm testing_Regular_"$sensor"_cutnorm_$3.txt &> /dev/null
-	rm testing_SemiRegular_"$sensor"_cutnorm_$3.txt &> /dev/null
-	rm testing_Irregular_"$sensor"_cutnorm_$3.txt &> /dev/null
-	rm training_Regular_"$sensor"_cutnorm_$3.txt &> /dev/null
-	rm training_SemiRegular_"$sensor"_cutnorm_$3.txt &> /dev/null
-	rm training_Irregular_"$sensor"_cutnorm_$3.txt &> /dev/null
+	rm testingfold$3_Regular_"$sensor"_cutnorm.txt &> /dev/null
+	rm testingfold$3_SemiRegular_"$sensor"_cutnorm.txt &> /dev/null
+	rm testingfold$3_Irregular_"$sensor"_cutnorm.txt &> /dev/null
+	rm trainingfold$3_Regular_"$sensor"_cutnorm.txt &> /dev/null
+	rm trainingfold$3_SemiRegular_"$sensor"_cutnorm.txt &> /dev/null
+	rm trainingfold$3_Irregular_"$sensor"_cutnorm.txt &> /dev/null
 done
 
 # get number of subjects in directory
@@ -47,28 +47,28 @@ fi
 fold_end=$(($subjects/$2*$3))
 fold_start=$(($fold_end-$subjects/$2+1))
 
-# create testing data
+# create testing3 data
 echo "Creating testing data..."
 for ((subject=$fold_start; subject<=$fold_end; subject++)); do
 	for ((sensor=1; sensor<=3; sensor++)); do
-		cat "$subject"_Regular_"$sensor"_cutnorm.txt >> testing_Regular_"$sensor"_cutnorm_$3.txt
-		cat "$subject"_SemiRegular_"$sensor"_cutnorm.txt >> testing_SemiRegular_"$sensor"_cutnorm_$3.txt
-		cat "$subject"_Irregular_"$sensor"_cutnorm.txt >> testing_Irregular_"$sensor"_cutnorm_$3.txt
+		cat "$subject"_Regular_"$sensor"_cutnorm.txt >> testingfold$3_Regular_"$sensor"_cutnorm.txt
+		cat "$subject"_SemiRegular_"$sensor"_cutnorm.txt >> testingfold$3_SemiRegular_"$sensor"_cutnorm.txt
+		cat "$subject"_Irregular_"$sensor"_cutnorm.txt >> testingfold$3_Irregular_"$sensor"_cutnorm.txt
 	done
 done
 
 # create training data
 echo "Creating training data..."
 for ((subject=1; subject<=$subjects; subject++)); do
-	# if not in the withheld testing subjects
+	# if not in the withheld testingfold$3 subjects
 	if (($subject<$fold_start || $subject>$fold_end)); then
 		for ((sensor=1; sensor<=3; sensor++)); do
-			cat "$subject"_Regular_"$sensor"_cutnorm.txt >> training_Regular_"$sensor"_cutnorm_$3.txt
-			cat "$subject"_SemiRegular_"$sensor"_cutnorm.txt >> training_SemiRegular_"$sensor"_cutnorm_$3.txt
-			cat "$subject"_Irregular_"$sensor"_cutnorm.txt >> training_Irregular_"$sensor"_cutnorm_$3.txt
+			cat "$subject"_Regular_"$sensor"_cutnorm.txt >> trainingfold$3_Regular_"$sensor"_cutnorm.txt
+			cat "$subject"_SemiRegular_"$sensor"_cutnorm.txt >> trainingfold$3_SemiRegular_"$sensor"_cutnorm.txt
+			cat "$subject"_Irregular_"$sensor"_cutnorm.txt >> trainingfold$3_Irregular_"$sensor"_cutnorm.txt
 		done
 	fi
 done
 
-echo "Done creating training and testing data files."
+echo "Done creating training and testingfold$3 data files."
 
