71d4b99001e13c6c264447ff238a53d52c3fbc72
acmilandroid <basilzhlin@gmail.com>
2020-11-05 10:14:16 -0500

    change 9_train to use kfold_train script

--- palmetto/launch_9_train.sh
+++ palmetto/launch_9_train.sh
@@ -1,10 +1,10 @@
-qsub -v "size=15" pbs/9_train.pbs
-qsub -v "size=30" pbs/9_train.pbs
-qsub -v "size=45" pbs/9_train.pbs
-qsub -v "size=60" pbs/9_train.pbs
-qsub -v "size=75" pbs/9_train.pbs
-qsub -v "size=90" pbs/9_train.pbs
-qsub -v "size=105" pbs/9_train.pbs
-qsub -v "size=120" pbs/9_train.pbs
-qsub -v "size=135" pbs/9_train.pbs
-qsub -v "size=150" pbs/9_train.pbs
+qsub -v "size=15, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=30, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=45, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=60, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=75, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=90, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=105, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=120, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=135, foldnum=5" pbs/kfold_train.pbs
+qsub -v "size=150, foldnum=5" pbs/kfold_train.pbs
