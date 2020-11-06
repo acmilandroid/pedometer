1601cb4e111466d57451071eab7a221bcd0a1225
acmilandroid <basilzhlin@gmail.com>
2020-11-06 00:07:29 -0500

    launch script and pbs script updates

--- cut/launch_cutnorm.sh
+++ cut/launch_cutnorm.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# Basil Lin
+# Step counter project
+# Launches cutnorm.sh 10 times, for each window 1-10 seconds
+
 ./cutnorm.sh ../PedometerData 15 1 0 &> /dev/null &
 ./cutnorm.sh ../PedometerData 30 1 0 &> /dev/null &
 ./cutnorm.sh ../PedometerData 45 1 0 &> /dev/null &
